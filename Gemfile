763e143086c83ba6a8324e5f77aa24294f077003
Arthur Cheek <artyrcheek@gmail.com>
2019-01-16 21:04:07 +0000

    dfsdfs

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org' do
   gem 'slack-ruby-client'
+  gem 'async-websocket'
   gem 'sinatra'
 end
--- slack-server.rb
+++ slack-server.rb
@@ -1,26 +1,35 @@
-require 'net/http'
-require 'json'
+# require 'net/http'
+# require 'json'
+# require 'slack-ruby-client'
+#
+# require 'sinatra'
+#
+# post '/whostracking' do
+#   content_type :json
+#   status 200
+#   # return return_currently_tracked_entries()
+#   # push = JSON.parse(request.body.read)
+#   # puts "I got some JSON: #{push.inspect}"
+#   return { "response_type": "in_channel", "text": "Its 80 degrees right now.", "attachments": [ { "text":"Partly cloudy today and tomorrow" } }
+# end
+#
+# get '/whostracking' do
+#   content_type :json
+#   response = { "text" => "hello there!"}
+#   status 200
+#   response.to_json
+#
+# end
+#
+# get '/' do
+#   return "yeet"
+# end
+
 require 'slack-ruby-client'
 
-require 'sinatra'
-
-post '/whostracking' do
-  content_type :json
-  status 200
-  # return return_currently_tracked_entries()
-  # push = JSON.parse(request.body.read)
-  # puts "I got some JSON: #{push.inspect}"
-  return { "response_type": "in_channel", "text": "Its 80 degrees right now.", "attachments": [ { "text":"Partly cloudy today and tomorrow" } }
+Slack.configure do |config|
+  config.token = ENV['6KpHP47cvIhGD0jUoTUegIRI']
 end
 
-get '/whostracking' do
-  content_type :json
-  response = { "text" => "hello there!"}
-  status 200
-  response.to_json
-
-end
-
-get '/' do
-  return "yeet"
-end
+client = Slack::Web::Client.new
+client.auth_test
