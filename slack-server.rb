5a6126c15ba5a474ab8dc817c28e8c01083f160f
Arthur Cheek <artyrcheek@gmail.com>
2019-01-16 21:21:28 +0000

    dgsdgs

--- slack-server.rb
+++ slack-server.rb
@@ -1,14 +1,47 @@
 require 'net/http'
 require 'json'
 require 'slack-ruby-client'
-
 require 'sinatra'
 
-post '/whostracking' do
+def get_json_url_with_params(url, params)
+  uri = URI(url)
+  uri.query = URI.encode_www_form(params)
+  res = Net::HTTP.get_response(uri)
+  response_body = res.body.to_s
+  return JSON.parse(response_body)
+end
+# cards =  get_json_url_with_params('https://api.breeze.pm/v2/cards/', { :api_token => 'B7ULqZ4WueSY-uv-yCZq', :assigned_id => '41569' })
+
+
+def slack_callback(slack_data)
+  return_message = ""
+  i = 1
+  num_cards = 1
+  while num_cards > 0
+    puts "checking page #{i}"
+    cards =  get_json_url_with_params('https://api.breeze.pm/v2/cards/', { :api_token => 'B7ULqZ4WueSY-uv-yCZq', :page => i})
+    cards.each do |card|
+      # puts "card with id: #{card['id']} and name: #{card['name']}"
+      # puts "#{card['name']} in project: #{card['project']['name']}"
+      card["time_entries"].each do  |entry|
+        if entry["tracked"] == nil
+          return_message << "#{entry['user_name']} - #{card['name']} \n"
+        end
+      end
+    end
+    num_cards = cards.length
+    i+= 1
+  end
+  puts return_message
+end
+
+
+
+post "/whostracking" do
   content_type :json
-  # return return_currently_tracked_entries()
-  pp request.POST.inspect
-  return "hello"
+  slack_data = request.POST.inspect
+  slack_callback(slack_data)
+  return "one minute, gathering data"
 end
 
 get '/' do
