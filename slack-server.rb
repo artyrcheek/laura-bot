71fd723bd9c2c2059abd390e3bd762fb2c89de85
Arthur Cheek <artyrcheek@gmail.com>
2019-01-16 21:10:27 +0000

    dfsdfs

--- slack-server.rb
+++ slack-server.rb
@@ -7,7 +7,7 @@ require 'sinatra'
 post '/whostracking' do
   content_type :json
   # return return_currently_tracked_entries()
-  puts request.POST.inspect
+  puts "HERE!" + request.POST.inspect
   return "hello"
 end
 
