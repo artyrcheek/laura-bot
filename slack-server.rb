0175bc74f93b7bf9cc134b1dbc61fe37f05fc979
Arthur Cheek <artyrcheek@gmail.com>
2019-01-16 21:05:33 +0000

    ertert

--- slack-server.rb
+++ slack-server.rb
@@ -1,35 +1,15 @@
-# require 'net/http'
-# require 'json'
-# require 'slack-ruby-client'
-#
-# require 'sinatra'
-#
-# post '/whostracking' do
-#   content_type :json
-#   status 200
-#   # return return_currently_tracked_entries()
-#   # push = JSON.parse(request.body.read)
-#   # puts "I got some JSON: #{push.inspect}"
-#   return { "response_type": "in_channel", "text": "Its 80 degrees right now.", "attachments": [ { "text":"Partly cloudy today and tomorrow" } }
-# end
-#
-# get '/whostracking' do
-#   content_type :json
-#   response = { "text" => "hello there!"}
-#   status 200
-#   response.to_json
-#
-# end
-#
-# get '/' do
-#   return "yeet"
-# end
-
+require 'net/http'
+require 'json'
 require 'slack-ruby-client'
 
-Slack.configure do |config|
-  config.token = ENV['6KpHP47cvIhGD0jUoTUegIRI']
+require 'sinatra'
+
+post '/whostracking' do
+  content_type :json
+  # return return_currently_tracked_entries()
+  return "hello"
 end
 
-client = Slack::Web::Client.new
-client.auth_test
+get '/' do
+  return "yeet"
+end
