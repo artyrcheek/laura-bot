763e650e0e5d4cd00b3c216841d8b6fb7d077bea
Arthur Cheek <artyrcheek@gmail.com>
2019-01-16 21:57:52 +0000

    dfsdf

--- slack-server.rb
+++ slack-server.rb
@@ -44,19 +44,10 @@ post "/whostracking" do
   status 200
   slack_data = request.POST.inspect
   puts "starting new thread"
-  # cb = Thread.new do
-  #   puts "in thread"
-  #   slack_callback(slack_data)
-  # end
-
-  child_pid = Process.fork do
-    # hard work is done here...
-      puts "in thread"
-      slack_callback(slack_data)
-    Process.exit
-  end
-
-  Process.detach child_pid # No zombie process
+  Thread.start {
+    puts "in thread"
+    slack_callback(slack_data)
+  }
   return "one minute, gathering data"
 
 end
