f0f53960d8fd6ab97bb9d1437a6843fdba7c6d05
Arthur Cheek <artyrcheek@gmail.com>
2019-01-16 20:38:21 +0000

    adsads

--- slack-server.rb
+++ slack-server.rb
@@ -4,20 +4,20 @@ require 'slack-ruby-client'
 
 require 'sinatra'
 
+post '/whostracking' do
+  content_type :json
+  # return return_currently_tracked_entries()
+  push = JSON.parse(request.body.read)
+  puts "I got some JSON: #{push.inspect}"
+  return "hello"
+end
+
 get '/whostracking' do
   content_type :json
   response = { "text" => "hello there!"}
   status 200
   response.to_json
-end
 
-post '/whostracking' do
-  content_type :json
-  response = { "text" => "hello there!"}
-  status 200
-  push = JSON.parse(request.body.read)
-  puts "I got some JSON: #{push.inspect}"
-  response.to_json
 end
 
 get '/' do
