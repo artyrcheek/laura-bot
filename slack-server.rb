e5ea39ec821334c88ca0d8f3bfe3e2509ac22956
Arthur Cheek <artyrcheek@gmail.com>
2019-01-17 18:04:36 +0000

    dasads

--- slack-server.rb
+++ slack-server.rb
@@ -19,34 +19,35 @@ def slack_whos_tracking_callback(slack_data)
   return_attatchments = ""
   i = 1
   num_cards = 1
-  while num_cards > 0
-    puts "checking page #{i}"
-    cards =  get_json_url_with_params('https://api.breeze.pm/v2/cards/', { :api_token => 'B7ULqZ4WueSY-uv-yCZq', :page => i})
-    cards.each do |card|
-      # puts "card with id: #{card['id']} and name: #{card['name']}"
-      # puts "#{card['name']} in project: #{card['project']['name']}"
-      card["time_entries"].each do  |entry|
-        if entry["tracked"] == nil
-          # return_attatchments << "#{entry['user_name']} - #{card['name']} app.breeze.pm/cards/#{card['id']} \n"
-          return_attatchments << "{
-            'color': 'good',
-            'author_name': '#{entry['user_name']}',
-            'author_link': 'https://app.breeze.pm/tasks/board?utf8=%E2%9C%93&users%5B%5D=#{entry['user_id']}',
-            'title': '#{card['name']}',
-            'title_link': 'https://app.breeze.pm/cards/#{card['id']}/',
-            'text': '#{card['project']['name']}'
-          },"
-        end
-      end
-    end
-    num_cards = cards.length
-    i+= 1
-  end
+  # while num_cards > 0
+  #   puts "checking page #{i}"
+  #   cards =  get_json_url_with_params('https://api.breeze.pm/v2/cards/', { :api_token => 'B7ULqZ4WueSY-uv-yCZq', :page => i})
+  #   cards.each do |card|
+  #     # puts "card with id: #{card['id']} and name: #{card['name']}"
+  #     # puts "#{card['name']} in project: #{card['project']['name']}"
+  #     card["time_entries"].each do  |entry|
+  #       if entry["tracked"] == nil
+  #         # return_attatchments << "#{entry['user_name']} - #{card['name']} app.breeze.pm/cards/#{card['id']} \n"
+  #         return_attatchments << "{
+  #           'color': 'good',
+  #           'author_name': '#{entry['user_name']}',
+  #           'author_link': 'https://app.breeze.pm/tasks/board?utf8=%E2%9C%93&users%5B%5D=#{entry['user_id']}',
+  #           'title': '#{card['name']}',
+  #           'title_link': 'https://app.breeze.pm/cards/#{card['id']}/',
+  #           'text': '#{card['project']['name']}'
+  #         },"
+  #       end
+  #     end
+  #   end
+  #   num_cards = cards.length
+  #   i+= 1
+  # end
   return_attatchments ||= "{
     'color': 'warning',
     'title': 'No one is tracking time!',
   },"
-  HTTParty.post(slack_data['response_url'], body: "{'response_type':'in_channel', 'attachments': [#{ return_attatchments[0..-1] }] }")
+  # HTTParty.post(slack_data['response_url'], body: "{'response_type':'in_channel', 'attachments': [#{ return_attatchments[0..-1] }] }")
+  HTTParty.post(slack_data['response_url'], body: "{'response_type':'in_channel', 'text': 'yep im triggered!' }")
 end
 
 def slack_yesterdays_report_callback(slack_data)
