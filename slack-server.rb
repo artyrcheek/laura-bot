19fc645bc9f274219eeccae3055cc671b5e2656d
Arthur Cheek <artyrcheek@gmail.com>
2019-01-17 18:34:02 +0000

    adsadsads

--- slack-server.rb
+++ slack-server.rb
@@ -53,10 +53,11 @@ def slack_whos_tracking_callback(slack_data)
     i+= 1
   end
   HTTParty.post(slack_data['response_url'], body: "{'response_type':'in_channel', 'text': 'finished scanning with #{people_tracking} people tracking' }")
-  return_attatchments ||= "{
-    'color': 'warning',
-    'title': 'No one is tracking time!',
-  },"
+  if return_attatchments == ""
+    return_attatchments = "{
+      'color': 'warning',
+      'title': 'No one is tracking time!',
+    },"
   HTTParty.post(slack_data['response_url'], body: "{'response_type':'in_channel', 'attachments': [#{ return_attatchments[0..-1] }] }")
   # HTTParty.post(slack_data['response_url'], body: "{'response_type':'in_channel', 'text': 'yep im triggered!' }")
 end
