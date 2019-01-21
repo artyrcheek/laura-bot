d4c65b44ad8905807be2301909a90d6d6febc4a9
Arthur Cheek <artyrcheek@gmail.com>
2019-01-21 15:26:13 +0000

    fsddfs

--- slack-server.rb
+++ slack-server.rb
@@ -44,7 +44,7 @@ def slack_callback(slack_data)
   if return_attatchments == ""
     return_attatchments = "{ 'color': 'warning', 'title': 'No one is tracking time!' },"
   end
-  HTTParty.post(slack_data['response_url'], body: "{'response_type':'in_channel', 'text': '*Current Tracking Report* by #{slack_data['user_name']}', 'attachments': [#{ return_attatchments[0..-1] }] }")
+  HTTParty.post(slack_data['response_url'], body: "{'response_type':'in_channel', 'text': '*Current Tracking Report* from #{slack_data['user_name']}', 'attachments': [#{ return_attatchments[0..-1] }] }")
 end
 
 def slack_yesterdays_report_callback(slack_data)
