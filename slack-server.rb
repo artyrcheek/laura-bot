2faa37cd57c23cd18612356ff593029a404c5b98
Arthur Cheek <artyrcheek@gmail.com>
2019-02-17 20:22:41 +0000

    moved whos tracking to its owbn module

--- slack-server.rb
+++ slack-server.rb
@@ -11,33 +11,6 @@ require "./whos-tracking"
 #PERSONAL_ACCESS_TOKEN = ENV["568833.pt.WqVZaB62RnKFoiPrGWZ_63OcI8YT_SZ5ylgCfjLCuaAYRAGy-3IPNgaFEdQjeqpxTC2MOEGFKTgYx-LUG_fDVw"]
 #ACCOUNT_ID = ENV["486922"]
 
-def slack_whos_tracking_callback(slack_data)
-  return_attatchments = ""
-  reports_response = HTTParty.post(
-    "https://api.breeze.pm/reports?api_token=B7ULqZ4WueSY-uv-yCZq",
-    body: {"report_type" => "timetracking", "start_date" => "today" }
-  )
-  reports_response.each do | entry |
-    if entry['tracked'] == nil
-      card = HTTParty.get("https://api.breeze.pm/projects/#{entry["project_id"]}/cards/#{entry["card_id"]}.json?api_token=B7ULqZ4WueSY-uv-yCZq",)
-      project = HTTParty.get("https://api.breeze.pm/projects/#{entry["project_id"]}.json?api_token=B7ULqZ4WueSY-uv-yCZq",)
-      better_entry = card['time_entries'].select do |newentry| newentry['id'] == entry['id'] end[0]
-      return_attatchments << "{
-        'color': '#36a64f',
-        'author_name': '#{better_entry['user_name']}',
-        'author_link': 'https://app.breeze.pm/tasks/board?utf8=%E2%9C%93&users%5B%5D=#{better_entry['user_id']}',
-        'title': '#{card['name']}',
-        'title_link': 'https://app.breeze.pm/cards/#{card['id']}/',
-        'text': '#{project['name']}'
-      },"
-    end
-  end
-
-  if return_attatchments == ""
-    return_attatchments = "{ 'color': 'warning', 'title': 'No one is tracking time!' },"
-  end
-  HTTParty.post(slack_data['response_url'], body: "{'response_type':'in_channel', 'text': '*Current Tracking Report* from <@#{slack_data['user_id']}>', 'attachments': [#{ return_attatchments[0..-1] }] }")
-end
 
 def slack_report_callback(slack_data)
   # Parsing start date test in slack text
