71de4a0f385b84a124366e21e71414a9e7be868f
Arthur Cheek <artyrcheek@gmail.com>
2019-02-17 20:33:55 +0000

    moved report to its own module

--- slack-server-1.rb
+++ slack-server-1.rb
@@ -4,45 +4,14 @@ require 'slack-ruby-client'
 require 'sinatra'
 require 'httparty'
 
+require "./project-report"
+require "./whos-tracking"
+require "./report"
+
 # Harvest
 #PERSONAL_ACCESS_TOKEN = ENV["568833.pt.WqVZaB62RnKFoiPrGWZ_63OcI8YT_SZ5ylgCfjLCuaAYRAGy-3IPNgaFEdQjeqpxTC2MOEGFKTgYx-LUG_fDVw"]
 #ACCOUNT_ID = ENV["486922"]
 
-def get_json_url_with_params(url, params)
-  uri = URI(url)
-  uri.query = URI.encode_www_form(params)
-  res = Net::HTTP.get_response(uri)
-  response_body = res.body.to_s
-  return JSON.parse(response_body)
-end
-
-def slack_whos_tracking_callback(slack_data)
-  return_attatchments = ""
-  reports_response = HTTParty.post(
-    "https://api.breeze.pm/reports?api_token=B7ULqZ4WueSY-uv-yCZq",
-    body: {"report_type" => "timetracking", "start_date" => "today" }
-  )
-  reports_response.each do | entry |
-    if entry['tracked'] == nil
-      card = HTTParty.get("https://api.breeze.pm/projects/#{entry["project_id"]}/cards/#{entry["card_id"]}.json?api_token=B7ULqZ4WueSY-uv-yCZq",)
-      project = HTTParty.get("https://api.breeze.pm/projects/#{entry["project_id"]}.json?api_token=B7ULqZ4WueSY-uv-yCZq",)
-      better_entry = card['time_entries'].select do |newentry| newentry['id'] == entry['id'] end[0]
-      return_attatchments << "{
-        'color': '#36a64f',
-        'author_name': '#{better_entry['user_name']}',
-        'author_link': 'https://app.breeze.pm/tasks/board?utf8=%E2%9C%93&users%5B%5D=#{better_entry['user_id']}',
-        'title': '#{card['name']}',
-        'title_link': 'https://app.breeze.pm/cards/#{card['id']}/',
-        'text': '#{project['name']}'
-      },"
-    end
-  end
-
-  if return_attatchments == ""
-    return_attatchments = "{ 'color': 'warning', 'title': 'No one is tracking time!' },"
-  end
-  HTTParty.post(slack_data['response_url'], body: "{'response_type':'in_channel', 'text': '*Current Tracking Report* from <@#{slack_data['user_id']}>', 'attachments': [#{ return_attatchments[0..-1] }] }")
-end
 
 def slack_report_callback(slack_data)
   # Parsing start date test in slack text
@@ -208,7 +177,7 @@ post "/whostracking" do
   slack_data = request.POST
   Thread.new do
     begin
-      slack_whos_tracking_callback(slack_data)
+      WhosTrackingCallback.slack_reply(slack_data)
     rescue
       error_response = "{'text': 'Sorry, something went wrong before trying to read breeze'}"
       HTTParty.post(slack_data['response_url'], body: error_response)
@@ -231,4 +200,19 @@ post "/report" do
     end
   end
   return "Getting Report data"
+end
+
+post "/projectreport" do
+  content_type :json
+  status 200
+  slack_data = request.POST
+  Thread.new do
+    begin
+      ProjectReportCallback.slack_reply(slack_data)
+    rescue
+      error_response = "{'text': 'Sorry, something went wrong before trying to scan breeze project reports'}"
+      HTTParty.post(slack_data['response_url'], body: error_response)
+    end
+  end
+  return "Getting Project Report data"
 end
